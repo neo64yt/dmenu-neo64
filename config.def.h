e41b55fe0f1cb8a0aaef3da2336bcba361cab2b5
lazyneo-setups64 <naufalramli06@gmail.com>
2020-12-23 15:43:14 +0800

    First commit

--- config.def.h
+++ config.def.h
@@ -1,23 +1,44 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
+static int instant = 0;
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
+static int centered = 0;                    /* -c option; centers dmenu on screen */
+static int min_width = 500;                    /* minimum width when centered */
+static int colorprompt = 0;                /* -p  option; if 1, prompt uses SchemeSel, otherwise SchemeNorm */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"Fira Code:size=11", 
+	"Material Design Icons:size=13",
+	"Joypixels:size=12"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char col_bg[]          = "#272a34";
+static const char col_font[]        = "#ffffff";
+static const char col_sel[]         = "#06a284";
+static const char col_fontsel[]     = "#02231c";
+static const char col_hl[]          = "#c5e817";
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeNorm] = { col_font, col_bg },
+	[SchemeSel] = { col_fontsel, col_sel },
+	[SchemeSelHighlight] = { col_sel, col_hl },
+	[SchemeNormHighlight] = { col_hl, col_sel },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
-/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+/* -l and -g options; controls number of lines and columns in grid if > 0 */
 static unsigned int lines      = 0;
+static unsigned int columns    = 0;
+/* -h option; minimum height of a menu line */
+static unsigned int lineheight = 0;
+static unsigned int min_lineheight = 8;
 
 /*
  * Characters not considered part of a word while deleting words
  * for example: " /?\"&[]"
  */
 static const char worddelimiters[] = " ";
+
+/* Size of the window border */
+static unsigned int border_width = 0;
